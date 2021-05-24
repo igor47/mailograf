bf68821b13fc111e672fb0fb95673c2076e68e88
Igor Serebryany <igor47@moomers.org>
2021-05-24 16:26:01 -0500

    main: actually report email metrics to telegraf

--- mailograf/main.py
+++ mailograf/main.py
@@ -1,6 +1,11 @@
+from collections import Counter
+from typing import Any, Dict
+import socket
+import subprocess
+
 import typer
 
-from mailograf.notmuch import db
+from mailograf.notmuch import db, tag_counts
 
 app = typer.Typer()
 
@@ -16,3 +21,38 @@ def tags(inbox: bool = typer.Option(False, help="Only show tags in the inbox")):
     for tag in tags:
         typer.echo(tag)
 
+@app.command()
+def counts(inbox: bool = typer.Option(True, help="Only count messages in the inbox")):
+    """Show counts of messages by tag"""
+    typer.echo(typer.style("Counting messages....", fg="blue"))
+
+    tc = tag_counts("tag:inbox" if inbox else "date:1Y..")
+    for tag, count in tc.items():
+        if inbox and tag == "inbox":
+            continue
+
+        tag = typer.style(tag, fg="green")
+        typer.echo(f"\t{tag}: {count}")
+
+@app.command()
+def report():
+    """report email metrics to telegraf"""
+    tags = {
+        "user": subprocess.check_output("whoami").strip().decode("utf-8"),
+        "host": subprocess.check_output("hostname").strip().decode("utf-8"),
+    }
+
+    ct = tag_counts("tag:inbox")
+    for tag, count in ct.items():
+        report_measurement("emails", tags, tag, count)
+
+    typer.echo(typer.style("Counts reported", fg="green"))
+
+def report_measurement(mname: str, tags: Dict[str, Any], tag: str, count: int) -> None:
+    """report email count to telegraf"""
+    all_tags = dict({"tag": tag}, **tags)
+    tag_str = ",".join(f"{t}={v}" for t, v in all_tags.items())
+
+    line = f"{mname},{tag_str} count={count}"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(line.encode(), ("127.0.0.1", 8094))
--- mailograf/notmuch.py
+++ mailograf/notmuch.py
@@ -1,3 +1,19 @@
+from collections import Counter
+
 from notmuch import Query, Database
 
 db = Database(mode=Database.MODE.READ_ONLY)
+
+def tag_counts(query: str) -> Counter:
+    """counts messages by tag for given query"""
+    q = db.create_query(query)
+    messages = q.search_messages()
+
+    tc = Counter()
+    for msg in messages:
+        tc["total"] += 1
+        for tag in msg.get_tags():
+            tc[tag] += 1
+
+    return tc
+
